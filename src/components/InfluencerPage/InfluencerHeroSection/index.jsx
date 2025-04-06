8e51ed8b543cc0c07a8ed22b2463047dd4487899
adityawintech <aditya#19AI>
2025-04-06 10:39:31 +0530

    Aditya

--- src/components/InfluencerPage/InfluencerHeroSection/index.jsx
+++ src/components/InfluencerPage/InfluencerHeroSection/index.jsx
@@ -54,7 +54,7 @@ const InfluencerHeroSection = () => {
       >
         <div className="relative w-[280px] h-[320px] md:w-[320px] md:h-[380px] rounded-3xl overflow-hidden shadow-2xl border-4 border-white">
           <Image
-            src="/teams/rajnish-kumar.png" // ⬅️ Replace with actual image path (e.g. public/founder.jpg)
+            src="/teams/rajnish-kumar.jpg" // ⬅️ Replace with actual image path (e.g. public/founder.jpg)
             alt="Influencer Campaign Strategist"
             layout="fill"
             objectFit="cover"
