d0275c247fbbfa9e0853d1b6e4ed0bcca2cb3f66
adityawintech <webitya@gmail.com>
2025-04-17 08:59:50 +0530

    Aditya

--- src/components/CoursesPage/CourseEnrollStudents/CourseStudentsCardData/coursestudentcarddata.js
+++ src/components/CoursesPage/CourseEnrollStudents/CourseStudentsCardData/coursestudentcarddata.js
@@ -2,11 +2,11 @@
 const coursestudentCardData = [
     {
       id: 1,
-      name: "Satyam",
+      name: "Satyam Kumar",
       course: "Digital Marketing",
       slug: "satyam-digital-marketing",
       shortDesc: "Learning Digital Marketing from scratch",
-      image: "/aboutHero.webp"
+      image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1744860461/WhatsApp_Image_2025-04-17_at_08.50.40_7196e4fa_hiuhxf.jpg"
     },
     // Add more students
   ];
