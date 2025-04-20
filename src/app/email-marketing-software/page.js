b2f0f1ee67cd644d77770fac1c1f306490bde36a
adityawintech <webitya@gmail.com>
2025-04-20 21:40:51 +0530

    Aditya

--- src/app/email-marketing-software/page.js
+++ src/app/email-marketing-software/page.js
@@ -1,3 +1,4 @@
+"use client"
 import ReceiverInput from "@/components/EmailMarketingSoftware/ReceiverEmail"
 import SendEmailsFunction from "@/components/EmailMarketingSoftware/SendEmailsFunction"
 import SenderForm from "@/components/EmailMarketingSoftware/SenderForm"
