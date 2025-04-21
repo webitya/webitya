b93f7eb0c0a2741bad227a49d4c568a1ed099ace
adityawintech <webitya@gmail.com>
2025-04-21 20:01:17 +0530

    Aditya

--- src/components/HandiSketch/Data/HandiSketchData.js
+++ src/components/HandiSketch/Data/HandiSketchData.js
@@ -3,7 +3,7 @@ const handiSketchData = [
         slug: "realistic-portrait1",
         title: "Realistic Portrait",
         description: "Beautiful hand-drawn sketch.",
-        image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1745233347/art7_ixwxxe.jpg",
+        image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1745245577/art7-680654ae6c6f3_d4akt5.webp",
         actualPrice: 2000,
         discountedPrice: 1500
       }
@@ -12,15 +12,35 @@ const handiSketchData = [
         slug: "realistic-portrait2",
         title: "Realistic Portrait",
         description: "Beautiful hand-drawn sketch.",
-        image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1745233346/art5_swgx0m.jpg",
+        image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1745245576/art5-680654a83bb43_sucr5l.webp",
         actualPrice: 2000,
         discountedPrice: 1500
       },
+      
+    
+   
+      {
+        slug: "realistic-portrait7",
+        title: "Realistic Portrait",
+        description: "Beautiful hand-drawn sketch.",
+        image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1745233571/art4_okbjjg.webp",
+        actualPrice: 2000,
+        discountedPrice: 1500
+      },
+      {
+        slug: "realistic-portrait6",
+        title: "Realistic Portrait",
+        description: "Beautiful hand-drawn sketch.",
+        image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1745245558/art3_jzlsqz.webp",
+        actualPrice: 2000,
+        discountedPrice: 1500
+      }
+      ,
     {
         slug: "realistic-portrait3",
         title: "Realistic Portrait",
         description: "Beautiful hand-drawn sketch.",
-        image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1745233359/art3_pvoums.png",
+        image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1745245576/art6-680654a9d319b_qidijk.webp",
         actualPrice: 2000,
         discountedPrice: 1500
       },
@@ -29,7 +49,7 @@ const handiSketchData = [
         slug: "realistic-portrait4",
         title: "Realistic Portrait",
         description: "Beautiful hand-drawn sketch.",
-        image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1745233350/art1_ik7wce.jpg",
+        image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1745245576/art2-6806549b9a1dc_wp7laa.webp",
         actualPrice: 2000,
         discountedPrice: 1500
       },
@@ -37,29 +57,11 @@ const handiSketchData = [
         slug: "realistic-portrait5",
         title: "Realistic Portrait",
         description: "Beautiful hand-drawn sketch.",
-        image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1745233350/art6_unwd65.jpg",
+        image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1745245575/art1-6806549c647b7_r7p34c.webp",
         actualPrice: 2000,
         discountedPrice: 1500
       }
-      ,
-      {
-        slug: "realistic-portrait6",
-        title: "Realistic Portrait",
-        description: "Beautiful hand-drawn sketch.",
-        image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1745233571/art4_okbjjg.webp",
-        actualPrice: 2000,
-        discountedPrice: 1500
-      }
-      ,
-   
-      {
-        slug: "realistic-portrait7",
-        title: "Realistic Portrait",
-        description: "Beautiful hand-drawn sketch.",
-        image: "https://res.cloudinary.com/dxqthnbx7/image/upload/v1745233343/art2_uqygxw.jpg",
-        actualPrice: 2000,
-        discountedPrice: 1500
-      }
+    
       
       
   ];
