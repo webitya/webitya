360fc21be7fa1fac01396b2ea1b9944849def248
adityawintech <aditya#19AI>
2025-07-24 21:48:05 +0530

    Aditya

--- src/app/api/blogNewsletter/route.js
+++ src/app/api/blogNewsletter/route.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer"
 
 // Create transporter
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
